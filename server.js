e1eae6eb19ba73e30119629503e6e1b29f1376cf
AbdullahMohee <163440764+AbdullahMohee@users.noreply.github.com>
2026-04-27 03:41:01 +0300

    Initialize Express server with MongoDB connection

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// DB Connection
+mongoose.connect("mongodb://127.0.0.1:27017/ecommerce")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// Routes
+app.use("/api/products", require("./routes/productRoutes"));
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
